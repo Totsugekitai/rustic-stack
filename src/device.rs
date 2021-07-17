9d64a6d5affab07d665747c76e8de9b6eb465c01
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2021-07-17 18:33:48 +0900

    wip

--- src/device.rs
+++ src/device.rs
@@ -1,25 +1,44 @@
-use std::{fs, io, path};
+pub mod linux {
+    use once_cell::sync::OnceCell;
+    use std::fs::{read_dir, read_link, read_to_string, File};
+    use std::io;
+    use std::path::Path;
 
-fn read_mac_address(address_file: &path::Path) -> Result<String, io::Error> {
-    let mac_address = fs::read_to_string(address_file)?;
-    Ok(mac_address)
-}
+    static TAP_FILE: OnceCell<File> = OnceCell::new();
 
-pub fn get_mac_address_list() -> Result<Vec<String>, io::Error> {
-    let net_devices_root_dir = path::Path::new("/sys/class/net");
-    let entries = fs::read_dir(net_devices_root_dir)?;
-    let mut address_vec = Vec::new();
-    for entry in entries {
-        let entry = entry?;
-        // let metadata = entry.metadata()?;
-        // let path = entry.path().display().to_string();
-
-        if let Ok(symlink) = fs::read_link(entry.path()) {
-            let path = symlink.as_path();
-            let address_file_path = path.join("address");
-            let mac_address = read_mac_address(&address_file_path)?;
-            address_vec.push(mac_address);
-        }
+    fn read_mac_address(address_file: &Path) -> Result<String, io::Error> {
+        let mac_address = read_to_string(address_file)?;
+        Ok(mac_address)
     }
-    Ok(address_vec)
+
+    pub fn get_mac_address_list() -> Result<Vec<String>, io::Error> {
+        let net_devices_root_dir = Path::new("/sys/class/net");
+        let entries = read_dir(net_devices_root_dir)?;
+        let mut address_vec = Vec::new();
+        for entry in entries {
+            let entry = entry?;
+            // let metadata = entry.metadata()?;
+            // let path = entry.path().display().to_string();
+
+            if let Ok(symlink) = read_link(entry.path()) {
+                let path = symlink.as_path();
+                let address_file_path = path.join("address");
+                let mac_address = read_mac_address(&address_file_path)?;
+                address_vec.push(mac_address);
+            }
+        }
+        Ok(address_vec)
+    }
+
+    pub fn open_tap() -> io::Result<()> {
+        let tap_path = Path::new("/dev/net/tap");
+        let tap_file = File::open(tap_path)?;
+        TAP_FILE.set(tap_file).unwrap();
+        Ok(())
+    }
+
+    pub fn poll_tap() {}
+    pub fn read_tap() {}
+    pub fn write_tap() {}
+    pub fn close_tap() {}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,8 @@ pub mod packet;
 mod tests {
     #[test]
     fn device() {
-        if let Ok(mac_addresses) = crate::device::get_mac_address_list() {
+        use crate::device::linux;
+        if let Ok(mac_addresses) = linux::get_mac_address_list() {
             for mac_address in mac_addresses {
                 eprintln!("{}", mac_address);
             }
