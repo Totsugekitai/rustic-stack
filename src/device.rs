f68dde8138dbb2c71b69aff6e66f796f94763203
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2021-07-19 17:45:20 +0900

    wip

--- src/device.rs
+++ src/device.rs
@@ -2,7 +2,7 @@ pub mod linux {
     use crate::ethernet::MacAddress;
     use ifstructs::ifreq;
     use libc;
-    use once_cell::sync::OnceCell;
+    // use once_cell::sync::OnceCell;
     use std::fs::{read_dir, read_link, read_to_string, File};
     use std::io;
     use std::os::unix::io::{AsRawFd, RawFd};
@@ -30,70 +30,70 @@ pub mod linux {
         Ok(address_vec)
     }
 
-    struct Epoll {
-        fd: RawFd,
-        events: Vec<libc::epoll_event>,
-    }
+    // struct Epoll {
+    //     fd: RawFd,
+    //     events: Vec<libc::epoll_event>,
+    // }
 
-    enum EpollEventType {
-        In,
-        Out,
-    }
+    // enum EpollEventType {
+    //     In,
+    //     Out,
+    // }
 
-    impl Epoll {
-        fn new(max_event: usize) -> Self {
-            let fd = unsafe { libc::epoll_create1(libc::EPOLL_CLOEXEC) };
-            let event: libc::epoll_event = unsafe { std::mem::zeroed() };
-            let events = vec![event; max_event];
-            Epoll { fd, events }
-        }
+    // impl Epoll {
+    //     fn new(max_event: usize) -> Self {
+    //         let fd = unsafe { libc::epoll_create1(libc::EPOLL_CLOEXEC) };
+    //         let event: libc::epoll_event = unsafe { std::mem::zeroed() };
+    //         let events = vec![event; max_event];
+    //         Epoll { fd, events }
+    //     }
 
-        fn add_event(&self, fd: RawFd, op: EpollEventType) {
-            let mut event: libc::epoll_event = unsafe { std::mem::zeroed() };
-            event.u64 = fd as u64;
-            event.events = match op {
-                EpollEventType::In => libc::EPOLLIN as u32,
-                EpollEventType::Out => libc::EPOLLOUT as u32,
-            };
+    //     fn add_event(&self, fd: RawFd, op: EpollEventType) {
+    //         let mut event: libc::epoll_event = unsafe { std::mem::zeroed() };
+    //         event.u64 = fd as u64;
+    //         event.events = match op {
+    //             EpollEventType::In => libc::EPOLLIN as u32,
+    //             EpollEventType::Out => libc::EPOLLOUT as u32,
+    //         };
 
-            unsafe { libc::epoll_ctl(self.fd, libc::EPOLL_CTL_ADD, fd, &mut event as *mut _) };
-        }
+    //         unsafe { libc::epoll_ctl(self.fd, libc::EPOLL_CTL_ADD, fd, &mut event as *mut _) };
+    //     }
 
-        fn mod_event(&self, fd: RawFd, op: EpollEventType) {
-            let mut event: libc::epoll_event = unsafe { std::mem::zeroed() };
-            event.u64 = fd as u64;
-            event.events = match op {
-                EpollEventType::In => libc::EPOLLIN as u32,
-                EpollEventType::Out => libc::EPOLLOUT as u32,
-            };
+    //     fn mod_event(&self, fd: RawFd, op: EpollEventType) {
+    //         let mut event: libc::epoll_event = unsafe { std::mem::zeroed() };
+    //         event.u64 = fd as u64;
+    //         event.events = match op {
+    //             EpollEventType::In => libc::EPOLLIN as u32,
+    //             EpollEventType::Out => libc::EPOLLOUT as u32,
+    //         };
 
-            unsafe { libc::epoll_ctl(self.fd, libc::EPOLL_CTL_MOD, fd, &mut event as *mut _) };
-        }
+    //         unsafe { libc::epoll_ctl(self.fd, libc::EPOLL_CTL_MOD, fd, &mut event as *mut _) };
+    //     }
 
-        fn del_event(&self, fd: RawFd) {
-            unsafe {
-                libc::epoll_ctl(
-                    self.fd,
-                    libc::EPOLL_CTL_DEL,
-                    fd,
-                    std::ptr::null_mut() as *mut _,
-                )
-            };
-        }
+    //     fn del_event(&self, fd: RawFd) {
+    //         unsafe {
+    //             libc::epoll_ctl(
+    //                 self.fd,
+    //                 libc::EPOLL_CTL_DEL,
+    //                 fd,
+    //                 std::ptr::null_mut() as *mut _,
+    //             )
+    //         };
+    //     }
 
-        fn wait(&mut self) -> usize {
-            let nfd = unsafe {
-                libc::epoll_wait(
-                    self.fd,
-                    self.events.as_mut_ptr(),
-                    self.events.len() as i32,
-                    -1, // no timeout
-                )
-            };
+    //     fn wait(&mut self) -> usize {
+    //         let nfd = unsafe {
+    //             libc::epoll_wait(
+    //                 self.fd,
+    //                 self.events.as_mut_ptr(),
+    //                 self.events.len() as i32,
+    //                 -1, // no timeout
+    //             )
+    //         };
 
-            nfd as usize
-        }
-    }
+    //         nfd as usize
+    //     }
+    // }
 
     #[derive(Debug)]
     pub struct Tap {
@@ -103,19 +103,10 @@ pub mod linux {
         mac_address: MacAddress,
     }
 
-    static TAP: OnceCell<Tap> = OnceCell::new();
+    // static TAP: OnceCell<Tap> = OnceCell::new();
 
     impl Tap {
-        fn new() -> Self {
-            Tap {
-                fd: -1,
-                name: String::new(),
-                ip_address: [0; 4],
-                mac_address: MacAddress::default(),
-            }
-        }
-
-        pub fn register(tap_name: &str) -> io::Result<()> {
+        pub fn open(tap_name: &str) -> io::Result<Tap> {
             let tap_file = File::open(Path::new("/dev/net/tun"))?;
 
             let mut ifr = ifreq::from_name(tap_name)?;
@@ -127,64 +118,45 @@ pub mod linux {
                 panic!("TAP allocation is failed");
             }
 
-            let tap_struct = Tap {
+            let tap = Tap {
                 fd: tap_fd,
                 name: String::from(tap_name),
                 ip_address: [192, 0, 2, 1],
                 mac_address: MacAddress::from_str("00:00:5e:00:53:FF").unwrap(),
             };
-            TAP.set(tap_struct).unwrap();
 
-            Ok(())
+            Ok(tap)
         }
-    }
 
-    pub const RW_BUF_SIZE: usize = 2 << 10;
+        pub fn close(&self) {
+            unsafe { libc::close(self.fd) };
+        }
 
-    pub fn poll(v: &mut [u8; RW_BUF_SIZE], size: usize) {
-        let mut epoll = Epoll::new(2); // ここの引数はテキトー
+        fn read(&self, buf: *mut u8, size: usize) -> isize {
+            unsafe { libc::read(self.fd, buf as *mut libc::c_void, size as _) as isize }
+        }
 
-        loop {
-            let nfd = epoll.wait();
+        fn write(&self, data: *const u8, size: usize) -> isize {
+            unsafe { libc::write(self.fd, data as *const libc::c_void, size as _) as isize }
+        }
 
-            for i in 0..nfd {
-                let fd = epoll.events[i].u64 as RawFd;
-                let events = epoll.events[i].events as i32;
+        pub fn poll(&self, buf: *mut u8, size: usize) -> isize {
+            let mut pfd = libc::pollfd {
+                fd: self.fd,
+                events: libc::POLLIN,
+                revents: 0,
+            };
 
-                if (events & libc::EPOLLIN) > 0 {
-                    let n = read_tap(v, size);
-                    if n == 0 {
-                        epoll.del_event(fd);
-                        break;
-                    }
-                } else if (events & libc::EPOLLOUT) > 0 {
-                    let _n = write_tap(v, size);
-                }
+            let ret = unsafe { libc::poll(&mut pfd as *mut libc::pollfd, 1, 3000) }; // 3sec待つ
+            if ret < 1 {
+                eprintln!("poll failed");
             }
-        }
-    }
 
-    pub fn read_tap(buf: &mut [u8; RW_BUF_SIZE], size: usize) -> isize {
-        unsafe {
-            libc::read(
-                TAP.get().unwrap().fd,
-                buf as *mut _ as *mut libc::c_void,
-                size as _,
-            ) as isize
+            self.read(buf, size)
         }
-    }
 
-    pub fn write_tap(data: &mut [u8; RW_BUF_SIZE], size: usize) -> isize {
-        unsafe {
-            libc::write(
-                TAP.get().unwrap().fd,
-                data as *mut _ as *mut libc::c_void,
-                size as _,
-            ) as isize
+        pub fn transmit(&self, data: *const u8, size: usize) -> isize {
+            self.write(data, size)
         }
-    }
-
-    pub fn close_tap() {
-        unsafe { libc::close(TAP.get().unwrap().fd) };
     }
 }
